ceb38ca9bc2dae5f2ba36b5f2061361b78e40bf5
vtr <vtr@DESKTOP-4JH9Q28>
2024-11-05 05:45:14 -0300

    Adicionar arquivos de projeto.

---
+++ MODULOAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MODULOAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDataHora()
+        {
+            var obj = new
+            {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToLongTimeString()
+            };
+
+            return Ok(obj);
+        }
+
+        [HttpGet("Apresentar/{nome}")]
+        public IActionResult Apresentar(string nome)
+        {
+            var mensagem = $"Ola {nome}, seja bem vindo!";
+            return Ok(new { mensagem });
+        }
+    }
+}
